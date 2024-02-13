0424686b16309247a4ab9e86795dc897c516a8f2
TanyaCST <schen@smith.edu>
2024-02-12 20:36:29 -0500

    Finished mirror words exchanging thing and most of other parts. Transcript awaiting.

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,105 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class Conversation {
+  public static String[] canned_responses = {
+    "Mmmhmmm",
+    "Interesting. Tell me more.",
+    "Nah, I don't understand what you are talking about.",
+    "I'm gonna go! Tom is coming.",
+    "Wow. That's hilarious."
+  };
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+
+    // Let users input the number of rounds.
+    Scanner sc = new Scanner(System.in);
+
+    // Declare an variable for greeting
+    String instruction = "Please enter the number of conversation rounds you want to go through:";
+    System.out.println(instruction);
+
+    // Create an array to store transcript
+    String[] transcript = {};
+
+    int num_rounds = sc.nextInt();
+
+    String greeting = "Hi! I'm chatbox 8546797. What's in your mind?";
+    System.out.println(greeting);
+
+    // Use a while loop to go through the conversation.
+    int i = 1;
+
+    while (i <= num_rounds) {
+
+      // Let users input things they want to ask.
+      String user_input = sc.nextLine();
+      System.out.println(user_input);
+
+      String response;
+
+      // Check mirror words 
+      if(user_input.contains("I")) {
+        response = user_input.replace("I", "you");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("me")){
+        response =  user_input.replace("me", "you");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("my")){
+        response = user_input.replace("my", "your");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("am")){
+        response = user_input.replace("am", "are");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("You")){
+        response = user_input.replace("You", "I");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("you")){
+        response = user_input.replace("you", "me");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("your")){
+        response = user_input.replace("your", "my");
+        System.out.println(response);
+      }
+
+      else if(user_input.contains("are")){
+        response = user_input.replace("are", "am");
+        System.out.println(response);
+      }
+
+      else{
+        // output canned responses
+        Random rand = new Random();
+        int indexCR = rand.nextInt();
+        response = canned_responses[indexCR];
+        System.out.println(response);
+
+      }
+      //transcript.add(response);
+
+      // Modify i
+      i++;
+
+      // After each loop store everything? 
+
+    }
+
+    // transcript.add() print transcript, use for loop 
+
+
+    sc.close();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,19 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Emily Ren
+
 - Which session(s) of TA / office hours did you attend?
+    Monday Feb 12th
+
 - What are your initial impressions of Java? 
+    The syntax matters!!!
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    Some questions I encountered when listing the outline/framework of this assignment:
+        1. What is a static array? How can we create it? How to store things in it? 
+            Static array is an array that the content inside doesn't change. Static means no changes happen
+        2. How to check whether certain characters/mirror words are in the string or not?
+        3. How to replace the mirror words?

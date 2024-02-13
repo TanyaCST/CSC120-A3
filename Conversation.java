bd3b757c96d740d570850995041cf0c9ae3bbff8
TanyaCST <schen@smith.edu>
2024-02-12 21:18:02 -0500

    Coding done for all, haven't test and debug yet.

--- Conversation.java
+++ Conversation.java
@@ -30,78 +30,73 @@ class Conversation {
     String greeting = "Hi! I'm chatbox 8546797. What's in your mind?";
     System.out.println(greeting);
 
-    // Use a while loop to go through the conversation.
-    int i = 1;
-
-    while (i <= num_rounds) {
-
+    for (int i =1; i <= num_rounds; i++) {
       // Let users input things they want to ask.
       String user_input = sc.nextLine();
       System.out.println(user_input);
-
+  
       String response;
-
+  
       // Check mirror words 
       if(user_input.contains("I")) {
         response = user_input.replace("I", "you");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("me")){
         response =  user_input.replace("me", "you");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("my")){
         response = user_input.replace("my", "your");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("am")){
         response = user_input.replace("am", "are");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("You")){
         response = user_input.replace("You", "I");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("you")){
         response = user_input.replace("you", "me");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("your")){
         response = user_input.replace("your", "my");
         System.out.println(response);
       }
-
+  
       else if(user_input.contains("are")){
         response = user_input.replace("are", "am");
         System.out.println(response);
       }
-
+  
       else{
         // output canned responses
         Random rand = new Random();
         int indexCR = rand.nextInt();
         response = canned_responses[indexCR];
         System.out.println(response);
-
+  
       }
       transcript.add(response);
+        
+        // After each loop store everything? 
+  
+      }
 
-      // Modify i
-      i++;
-
-      // After each loop store everything? 
-
-    }
-
-    // transcript.add() print transcript, use for loop 
-
+      for(int j = 0; j <= transcript.size(); j++){
+        System.out.println(transcript.get(j));
+      }
 
     sc.close();
   }
+  
 }
